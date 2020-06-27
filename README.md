9f668b6f806e9cd3d037f4edac128be0035699da
Creatoria <1612706077@qq.com>
2020-06-27 16:13:16 +0800

    初次

---
+++ README.md
@@ -0,0 +1,18 @@
+# SageMath 中文文档翻译
+尝试对SageMath上的英文文档进行翻译，当前最新版本为SageMath 9.1, May 21,2020。
+预期目标是将Tutorial与Reference里的接口文档翻译完，并使用markdown格式整理。
+- [ ] 整合已有的Tutorial的中文文档并补充、跟随更新
+- [ ] 逐步翻译reference的接口文档
+## 进度
+- Tutorial: 剩余第五章部分内容未完成
+- Reference: 未开始
+## 源文档获取
+可以从[这里](https://github.com/sagemath/documentation)获取到SageMath的最新版本文档(PDF或HTML格式)，目前版本为9.1。
+## 参考
+在整合Toturial文档的过程中使用了以下来源的翻译:
+- [amao](http://ai7.org/wp/html/682.html) CC-BY-SA 4.0
+- [Lainme](https://www.lainme.com/doku.php/topic/sage/start) CC-BY-SA 3.0
+
+## License
+MIT
+CC-BY-SA 4.0
---
+++ docinit.py
@@ -0,0 +1,44 @@
+import fitz
+import os
+import sys
+
+
+def toc2chapter(toc):
+    di = []
+    st = -1
+    for t in toc:
+        if t[0] == 1:
+            di.append([t[1]])
+            st += 1
+        else:
+            di[st].append(t[1])
+    return di
+
+
+filename = sys.argv[1]
+filename = os.path.basename(filename)
+filenoext = '.'.join(filename.split('.')[:-1])
+basedir = os.path.dirname(filename)
+workdir = basedir + filenoext
+if not os.path.exists(workdir):
+    os.mkdir(workdir)
+    os.mkdir(workdir + '/Markdown')
+p = fitz.open(filename)
+toc = p.getToC()
+# chp = [t[1].strip().replace(' ', '_') for t in toc if t[0] == 1]
+# for i in range(1, len(chp) + 1):
+
+d = toc2chapter(toc)
+ii = 0
+for ch in d:
+    ii += 1
+    jj = 0
+    tmpdir = workdir + '/Markdown/CH' + str(ii).zfill(2)+'/'
+    if not os.path.exists(tmpdir):
+        os.mkdir(tmpdir)
+    for page in ch:
+        
+        name = str(ii).zfill(2) + '.' + str(jj) + '_' + \
+            page.strip().replace('/',' ').replace('?',' ').replace(' ', '_') + '.md'
+        open(tmpdir + name, 'w').close()
+        jj += 1
